d0f37ce68f404c185d19f05c4cd11f5241e57c92
Yuriy Tumakha <tumakha@gmail.com>
2018-03-16 20:41:15 +0000

    acronym

--- exercises/acronym/src/main/scala/Acronym.scala
+++ exercises/acronym/src/main/scala/Acronym.scala
@@ -1,3 +1,4 @@
 object Acronym {
-  def abbreviate(phrase: String): String = ???
+  def abbreviate(phrase: String): String =
+    phrase.split("\\W+").map(_.head.toUpper).mkString("")
 }
--- exercises/acronym/src/test/scala/AcronymTest.scala
+++ exercises/acronym/src/test/scala/AcronymTest.scala
@@ -8,22 +8,19 @@ class AcronymTest extends FunSuite with Matchers {
   }
 
   test("lowercase words") {
-    pending
     Acronym.abbreviate("Ruby on Rails") should be ("ROR")
   }
 
   test("punctuation") {
-    pending
     Acronym.abbreviate("First In, First Out") should be ("FIFO")
   }
 
   test("all caps word") {
-    pending
     Acronym.abbreviate("GNU Image Manipulation Program") should be ("GIMP")
   }
 
   test("punctuation without whitespace") {
-    pending
     Acronym.abbreviate("Complementary metal-oxide semiconductor") should be ("CMOS")
   }
+
 }
