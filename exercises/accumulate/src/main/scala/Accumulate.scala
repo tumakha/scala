bd4370bf3935a22075d2860091f1d8b936571f12
Yuriy Tumakha <tumakha@gmail.com>
2018-03-16 20:15:05 +0000

    accumulate

--- exercises/accumulate/src/main/scala/Accumulate.scala
+++ exercises/accumulate/src/main/scala/Accumulate.scala
@@ -1,3 +1,15 @@
+import scala.annotation.tailrec
+
 class Accumulate {
-  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = ???
+
+  def accumulate[A, B](f: (A) => B, list: List[A]): List[B] = {
+    @tailrec def acc(input: List[A], result: List[B]): List[B] =
+      input match {
+        case xs :+ x => acc(xs, f(x) :: result)
+        case Nil => result
+      }
+
+    acc(list, Nil)
+  }
+
 }
